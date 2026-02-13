00e71b88783f574df81adaca17a6704f09671c83
Pedro Macedo Amorim <pedrok.amorim07@gmail.com>
2026-02-13 10:30:29 -0300

    Class Client created

---
+++ entities/Client.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.util.Date;
+
+public class Client {
+
+	private String name;
+	private String email;
+	private Date birthDate;
+	
+	public Client(String name, String email, Date birthDate) {
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	
+}
